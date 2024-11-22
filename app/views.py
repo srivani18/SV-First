d2804529dd9ef88310b1e449d23fbbc400fc73ef
Your Name <you@example.com>
2024-11-22 16:24:34 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def vani(request):
+    return HttpResponse('UNIVERSE ALWAYS FALLS IN LOVE WITH STUBBORN HEART')
+
+
